e3e4b03f1261b685b73e2d0ec8f95018254bc3c8
Daniel Große <daniel.grosse@diginetmedia.de>
2018-01-03 09:06:20 +0100

    Merged Master

--- config.js
+++ config.js
@@ -1,89 +1,88 @@
 module.exports = {
   brew: [
     // http://conqueringthecommandline.com/book/ack_ag
-    'ack',
-    'ag',
-    // Install GNU core utilities (those that come with OS X are outdated)
+    "ack",
+    "ag",
+    // Install GNU core utilities (those that come with macOS are outdated)
     // Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-    'coreutils',
-    'dos2unix',
+    "coreutils",
+    "dos2unix",
     // Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-    'findutils',
-    'fortune',
-    'readline', // ensure gawk gets good readline
-    'gawk',
+    "findutils",
+    "fortune",
+    "readline", // ensure gawk gets good readline
+    "gawk",
     // http://www.lcdf.org/gifsicle/ (because I'm a gif junky)
-    'gifsicle',
-    'gnupg',
+    "gifsicle",
+    "gnupg",
     // Install GNU `sed`, overwriting the built-in `sed`
     // so we can do "sed -i 's/foo/bar/' file" instead of "sed -i '' 's/foo/bar/' file"
-    'gnu-sed --with-default-names',
+    "gnu-sed --with-default-names",
     // better, more recent grep
-    'homebrew/dupes/grep',
+    "homebrew/dupes/grep",
     // https://github.com/jkbrzt/httpie
-    'httpie',
+    "httpie",
     // jq is a sort of JSON grep
-    'jq',
+    "jq",
     // Mac App Store CLI: https://github.com/mas-cli/mas
-    'mas',
+    "mas",
     // Install some other useful utilities like `sponge`
-    'moreutils',
-    'nmap',
-    'openconnect',
-    'reattach-to-user-namespace',
+    "moreutils",
+    "nmap",
+    "openconnect",
+    "reattach-to-user-namespace",
     // better/more recent version of screen
-    'homebrew/dupes/screen',
-    'tmux',
-    'tree',
-    'ttyrec',
+    "homebrew/dupes/screen",
+    "tmux",
+    "tree",
+    "ttyrec",
     // better, more recent vim
-    'vim --with-override-system-vi',
-    'watch',
+    "vim --with-override-system-vi",
+    "watch",
     // Install wget with IRI support
-    'wget --enable-iri',
-    'git-extras'
+    "wget --enable-iri",
+    "git-extras"
   ],
   cask: [
     //'adium',
     //'amazon-cloud-drive',
     //'atom',
-    // 'box-sync',
+    //'box-sync',
     //'comicbooklover',
     //'diffmerge',
-    'dropbox',
+    "dropbox",
     //'evernote',
-    'gpgtools',
+    "gpgtools",
     //'ireadfast',
-    'iterm2',
-    'little-snitch',
-    'micro-snitch',
+    "iterm2",
+    "little-snitch",
+    "micro-snitch",
     //'macvim',
-    'sizeup',
+    "sizeup",
     //'sketchup',
-    'slack',
-    'the-unarchiver',
+    "slack",
+    "the-unarchiver",
     //'torbrowser',
     //'transmission',
-    'vlc',
-    'xquartz'
+    "vlc",
+    "xquartz"
   ],
-  gem: [
-    'git-up'
-  ],
+  gem: ["git-up"],
+  gem: ["git-up"],
   npm: [
-    'antic',
-    'buzzphrase',
-    'eslint',
-    'instant-markdown-d',
-    'npm-check',
-    'yo',
-    'grunt',
+    "antic",
+    "buzzphrase",
+    "eslint",
+    "instant-markdown-d",
+    "npm-check",
+    "yo",
+    "grunt",
     // 'generator-dockerize',
-    'gulp',
-    'npm-check',
-    'prettyjson',
-    'trash',
-    'vtop'
+    "gulp",
+    "npm-check",
+    "prettyjson",
+    "trash",
+    "vtop"
     // ,'yo'
   ]
 };
